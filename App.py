038ebe9491f571e40ad478f70498ef84e8055d2e
Melvin-Leroy <melv.leroy@gmail.com>
2021-01-26 12:25:15 +0100

    Update App.py

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ import altair as alt
 
 @st.cache
 def get_data():
-    df = pd.read_csv('data/COVID-19-geographic-disbtribution-worldwide.xlsx', keep_default_na=False, na_values='', engine="openpyxl")
+    df = pd.read_excel('data/COVID-19-geographic-disbtribution-worldwide.xlsx', keep_default_na=False, na_values='', engine="openpyxl")
     df_geo = df.dropna(subset=['geoId']).pivot(index='dateRep', columns='geoId', values=['cases', 'deaths'])
     return df_geo
 
